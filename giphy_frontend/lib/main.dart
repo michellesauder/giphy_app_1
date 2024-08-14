428288924f29622651685e4c52278a19c8ed7e4c
Michelle S <mgsauder@hotmail.com>
2024-08-14 13:46:58 -0700

    clean up

--- giphy_frontend/lib/main.dart
+++ giphy_frontend/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart'; // Import Flutter material design package
-import 'package:http/http.dart'
-    as http; // Import HTTP package for making network requests
-import 'dart:convert'; // Import Dart's convert library for JSON encoding/decoding
 
+import 'pages/giphy_home.dart';
 
 void main() {
   runApp(MyApp()); // Run the MyApp widget as the root of the application
@@ -17,87 +15,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue, // Define the primary color theme
       ),
       home: GiphyHomePage(), // Set the home page of the app
-    );
-  }
-}
-
-class GiphyHomePage extends StatefulWidget {
-  @override
-  _GiphyHomePageState createState() =>
-      _GiphyHomePageState(); // Create the state for GiphyHomePage
-}
-
-class _GiphyHomePageState extends State<GiphyHomePage> {
-  List<dynamic> gifs = []; // Initialize an empty list to hold GIFs
-  final TextEditingController _controller =
-      TextEditingController(); // Controller for the text field
-
-  // Function to fetch GIFs based on the search query
-  Future<void> fetchGifs(String query) async {
-    // Make a GET request to the Giphy API with the search query
-    final response = await http
-        .get(Uri.parse('http://localhost:5000/api/gifs/search?query=$query'));
-
-    // Check if the response status is OK (200)
-    if (response.statusCode == 200) {
-      setState(() {
-        // Decode the JSON response and update the gifs list
-        gifs = json.decode(response.body)['data'];
-      });
-    } else {
-      // Throw an exception if the request failed
-      throw Exception('Failed to load GIFs');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Giphy App'), // Set the title of the app bar
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding:
-                const EdgeInsets.all(8.0), // Add padding around the text field
-            child: TextField(
-              controller:
-                  _controller, // Assign the controller to the text field
-              decoration: InputDecoration(
-                labelText: 'Search GIFs', // Label for the text field
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.search), // Search icon
-                  onPressed: () {
-                    // When the search icon is pressed, fetch GIFs using the text in the controller
-                    fetchGifs(_controller.text);
-                  },
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: gifs.isEmpty // Check if the gifs list is empty
-                ? const Center(
-                    child: Text(
-                        'No GIFs found')) // Show a message if no GIFs are found
-                : GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount:
-                          2, // Set the number of columns in the grid
-                    ),
-                    itemCount:
-                        gifs.length, // Set the number of items in the grid
-                    itemBuilder: (context, index) {
-                      // Build each grid item with an image from the GIFs list
-                      return Image.network(
-                          gifs[index]['images']['fixed_height']['url']);
-                    },
-                  ),
-          ),
-        ],
-      ),
     );
   }
 }
--- giphy_frontend/test/fetch_giphy_test.dart
+++ giphy_frontend/test/fetch_giphy_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:giphy_frontend/pages/giphy_home.dart';
 import 'package:http/http.dart' as http;
-import 'package:giphy_frontend/main.dart';
 import 'package:mockito/annotations.dart';
 
 // Generate a MockClient using the Mockito package.
@@ -21,33 +21,10 @@ void main() {
       expect(find.text('No GIFs found'), findsOneWidget);
     });
   });
+
+  
+
+
+
+
 }
-
-// void main() {
-//   group('fetchAlbum', () {
-//     test('returns an Album if the http call completes successfully', () async {
-//       final client = MockClient();
-
-//       // Use Mockito to return a successful response when it calls the
-//       // provided http.Client.
-//       when(client
-//               .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')))
-//           .thenAnswer((_) async =>
-//               http.Response('{"userId": 1, "id": 2, "title": "mock"}', 200));
-
-//       expect(await fetchAlbum(client), isA<Album>());
-//     });
-
-//     test('throws an exception if the http call completes with an error', () {
-//       final client = MockClient();
-
-//       // Use Mockito to return an unsuccessful response when it calls the
-//       // provided http.Client.
-//       when(client
-//               .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')))
-//           .thenAnswer((_) async => http.Response('Not Found', 404));
-
-//       expect(fetchAlbum(client), throwsException);
-//     });
-//   });
-// }
