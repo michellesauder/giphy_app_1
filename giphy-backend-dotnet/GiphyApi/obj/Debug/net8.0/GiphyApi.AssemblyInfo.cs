7a92c4e8b1f9f017b63f25c83ca3a6367e1cb137
Michelle S <mgsauder@hotmail.com>
2024-08-15 13:18:56 -0700

    Integration testing init

--- giphy-backend-dotnet/GiphyApi/obj/Debug/net8.0/GiphyApi.AssemblyInfo.cs
+++ giphy-backend-dotnet/GiphyApi/obj/Debug/net8.0/GiphyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GiphyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6788348032073a72767162744f2883046bcea630")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+990ddcc1c8b1f275931ffea05c38c542a529dbab")]
 [assembly: System.Reflection.AssemblyProductAttribute("GiphyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GiphyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- giphy_frontend/lib/main.dart
+++ giphy_frontend/lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart'; // Import Flutter material design package
-import 'package:http/http.dart'
-    as http; // Import HTTP package for making network requests
-
-import 'pages/giphy_home.dart';
+import 'package:giphy_frontend/pages/login_screen.dart';
 
 void main() {
   runApp(MyApp()); // Run the MyApp widget as the root of the application
@@ -16,9 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue, // Define the primary color theme
       ),
-      home: GiphyHomePage(
-        client: http.Client(),
-      ), // Set the home page of the app
+      home: LoginPage(), // Set the home page of the app
     );
   }
 }
